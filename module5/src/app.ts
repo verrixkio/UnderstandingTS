c61e9597ddba5a22edf2762c894267ae1d014d1a
Thomas <verrixkio@gmail.com>
2021-08-06 15:57:56 -0600

    Classes, interface, and compiling

--- module5/src/app.ts
+++ module5/src/app.ts
@@ -1,64 +1,19 @@
-// you can use abstract to mark a class, this forces you to use a certain method in all other inhertied classes.
+//an interface is used 
+interface Person {
+  name: string;
+  age: number;
 
-
-//Private constructor. Helpful to manage singleton pattern.
-
-
-class Department { 
-  private employees: string[] = [];
-
-  //Allows you to write functions that run on creation of the class.
-  //Shorthand for class propertoes like id and public. Can be declared elsewhere. 
-  constructor(private readonly id: string, public name: string) {
-  }
-
-  describe() {
-    console.log(`Department(${this.id}): + ${this.name}`);
-  }
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInfo() {
-    console.log(this.employees.length)
-    console.log(this.employees)
-  }
+  greet(phrase: string): void;
 }
 
-//extends keyword inherits from another class.
-class ITDepartment extends Department {
-  private lastReport: string;
+let user1: Person;
 
-  //Can add a getter to retieve something private from another class
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport
-    }
-    throw new Error('No report found.')
+user1 = {
+  name: "max",
+  age: 16,
+
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name)
   }
-
-  //Setter
-  set mostRecentReport(value: string) {
-    this.addReport(value);
-  }
-
-  constructor(id:string, public reports: string[]) {
-    // super calls the parent class constructor.
-    super(id, 'IT');
-    this.lastReport = reports[0];
-  }
-
-  addReport(text: string){
-    this.reports.push(text);
-    this.lastReport = text[0]
-  }
-
-  printReport() {
-    console.log(this.reports);
-  }
-}
-
-//Constructor is called at this point.
-const it = new ITDepartment('d1', []);
-console.log(it.mostRecentReport);
+};
+user1.greet("Hello there I am")
